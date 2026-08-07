64b66283585451b2c0019cef4e4a91d51abce671
Yusif İmranlı <imranliyusif2006@gmail.com>
2026-08-07 14:08:25 +0400

    install vercl

--- api/[name].js
+++ api/[name].js
@@ -1,19 +1,5 @@
-import { createRequire } from "module";
-
-const require = createRequire(import.meta.url);
-
-const newArrival = require("./data/newarrival.json");
-
 export default function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  const { name } = req.query;
-
-  if (name === "newarrival") {
-    return res.status(200).json(newArrival);
-  }
-
-  return res.status(404).json({
-    message: "Not found",
+  res.status(200).json({
+    route: req.query.name,
   });
 }
