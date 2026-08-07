4c13e74d787271dcc55a20c3c7e27cc4e41636b0
Yusif İmranlı <imranliyusif2006@gmail.com>
2026-08-07 12:20:31 +0400

    test name endpoint

--- api/[name].js
+++ api/[name].js
@@ -1,15 +1,5 @@
-import newArrival from "./data/newarrival.json" with { type: "json" };
-
 export default function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-
-  const { name } = req.query;
-
-  if (name === "newarrival") {
-    return res.status(200).json(newArrival);
-  }
-
-  return res.status(404).json({
-    message: "Not found",
+  res.status(200).json({
+    test: "name api works"
   });
 }
