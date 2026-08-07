c52d746a7691f887ea03741fee16f375d4a5da80
Yusif İmranlı <imranliyusif2006@gmail.com>
2026-08-07 12:18:33 +0400

    fix json import

--- api/[name].js
+++ api/[name].js
@@ -1,9 +1,15 @@
-import { createRequire } from "module";
-
-const require = createRequire(import.meta.url);
-
-const newArrival = require("./data/newarrival.json");
+import newArrival from "./data/newarrival.json" with { type: "json" };
 
 export default function handler(req, res) {
-  return res.status(200).json(newArrival);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  const { name } = req.query;
+
+  if (name === "newarrival") {
+    return res.status(200).json(newArrival);
+  }
+
+  return res.status(404).json({
+    message: "Not found",
+  });
 }
