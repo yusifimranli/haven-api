320e715065e2f376b18b1e1156a388bbbbe79b35
Yusif İmranlı <imranliyusif2006@gmail.com>
2026-08-07 12:15:03 +0400

    test api json

--- api/[name].js
+++ api/[name].js
@@ -1,21 +1,9 @@
-import newArrival from "./data/newarrival.json" with { type: "json" };
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+
+const newArrival = require("./data/newarrival.json");
 
 export default function handler(req, res) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-  if (req.method === "OPTIONS") {
-    return res.status(200).end();
-  }
-
-  const { name } = req.query;
-
-  if (name === "newarrival") {
-    return res.status(200).json(newArrival);
-  }
-
-  return res.status(404).json({
-    message: "Not found",
-  });
+  return res.status(200).json(newArrival);
 }
