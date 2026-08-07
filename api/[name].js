f3a47fafedede0dffa883020d1ea337685ba2eed
Yusif İmranlı <imranliyusif2006@gmail.com>
2026-08-07 12:25:58 +0400

    read json with fs

--- api/[name].js
+++ api/[name].js
@@ -1,8 +1,15 @@
-import newArrival from "./data/newarrival.json" with { type: "json" };
+import fs from "fs";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export default function handler(req, res) {
-  res.status(200).json({
-    length: newArrival.length,
-    first: newArrival[0]
-  });
+  const filePath = path.join(__dirname, "data", "newarrival.json");
+
+  const data = fs.readFileSync(filePath, "utf-8");
+  const newArrival = JSON.parse(data);
+
+  res.status(200).json(newArrival);
 }
